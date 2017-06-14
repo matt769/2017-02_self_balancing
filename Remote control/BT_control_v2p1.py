c2fbe758156e3b8f45e67c287656b170fea11f76
matt769 <matthedley@hotmail.co.uk>
2017-06-14 01:11:01 +0100

    removed old comments

--- Remote control/BT_control_v2p1.py
+++ Remote control/BT_control_v2p1.py
@@ -1,12 +1,3 @@
-#v2 CHANGES
-#just checks for any response
-
-#TO ADD
-# should i use serial.SerialException in except clause?
-# add some code to both this and receiver (in terms of answer/response) to confirm all is as expected
-# check if there's anything to receive before printing
-
-
 
 from msvcrt import getch
 import serial
@@ -35,23 +26,15 @@ while True:
 	if key == 'q':
 		print "Exiting program"
 		break
-	# don't send any more data to serial port if it's the same as before
-	# this could be a problem if I want to add a timeout on the receiver side
-	#if key == lastKey:
-	#	continue
 	# receiver will handle any unexpected characters
 	print 'New input:',key
 	ser.write(key)
-	#ser.write('\n')
 	lastKey = key
 	# print 'Response from receiver:',ser.read(size=4)
 
-# try sending kill command
-
 
 try:
-	ser.write('x')
-	#ser.write('\n')
+	#ser.write('x')
 	print "Sending final stop command"
 	ser.close()
 	print "Closing connection"
